4e364ffff87d5383f591c3fbec6dc3c09788161c
FranzMV <FranManzanedoValle@gmail.com>
2024-03-30 21:49:59 +0100

    securityConfig con SecurityFilterChain

---
+++ src/main/java/com/spring_security/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.spring_security.constants;
+
+public class SecurityConstants {
+
+    public static String NON_NEEDED_SECURITY_ENDPOINT ="/v1/index2";
+    public static String SECURITY_NEEDED_PATH="/v1/index";
+
+    public static String SESSION_PATH="/v1/session";
+}
