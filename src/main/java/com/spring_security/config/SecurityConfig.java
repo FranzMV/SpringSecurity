e7e07cddd8194f0cfaae736d79db570c08836322
FranzMV <FranManzanedoValle@gmail.com>
2024-03-31 21:04:16 +0200

    add BCryptPasswordEncoder

--- src/main/java/com/spring_security/config/SecurityConfig.java
+++ src/main/java/com/spring_security/config/SecurityConfig.java
@@ -12,18 +12,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -74,12 +67,11 @@ public class SecurityConfig {
     }
 
 
-
-
+    /**
+     * Password encoder with BCryptPasswordEncoder
+     * @return PasswordEncoder
+     */
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
-
+    public PasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();}
 
 }
