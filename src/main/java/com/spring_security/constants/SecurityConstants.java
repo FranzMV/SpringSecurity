6d8080170a74e065e99f3b3b4c6c0d57a03db51e
FranzMV <FranManzanedoValle@gmail.com>
2024-04-01 15:43:02 +0200

    refactor SecurityConstants.java

--- src/main/java/com/spring_security/constants/SecurityConstants.java
+++ src/main/java/com/spring_security/constants/SecurityConstants.java
@@ -3,6 +3,7 @@ package com.spring_security.constants;
 public class SecurityConstants {
 
     //ENDPOINT PATHS
+    public static final String SIGN_UP_PATH = "/sign-up";
     public static final String LOG_IN_PATH ="/log-in";
     public static final String METHOD_PATH ="/method";
     public static final String AUTH_PATH ="/auth";
@@ -14,10 +15,17 @@ public class SecurityConstants {
 
     //USER ROLES
     public static final String ADMIN_ROLE ="ADMIN";
-    public static final String REFACTOR_ROLE ="REFACTOR";
-    public static final String DEVELOPER_ROLE ="DEVELOPER";
     public static final String USER_ROLE ="USER";
     public static final String INVITED_ROLE ="INVITED";
+    public static final String DEVELOPER_ROLE ="DEVELOPER";
+
+    //PERMISSIONS
+    public static final String READ_PERMISSION ="READ";
+    public static final String CREATE_PERMISSION ="CREATE";
+    public static final String UPDATE_PERMISSION ="UPDATE";
+    public static final String DELETE_PERMISSION ="DELETE";
+    public static final String REFACTOR_PERMISSION ="REFACTOR";
+
 
     //CONF JWT
     public static final String ROLE_ ="ROLE_";
