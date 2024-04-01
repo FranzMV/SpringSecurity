b7368ec9fd3f66863fe7f817cb1dc8dd9eb260ae
FranzMV <FranManzanedoValle@gmail.com>
2024-04-01 13:54:31 +0200

    rename constants

--- src/main/java/com/spring_security/constants/SecurityConstants.java
+++ src/main/java/com/spring_security/constants/SecurityConstants.java
@@ -1,16 +1,16 @@
 package com.spring_security.constants;
 
-import org.springframework.web.bind.annotation.*;
-
 public class SecurityConstants {
 
     //ENDPOINT PATHS
+    public static final String LOG_IN_PATH ="/log-in";
+    public static final String METHOD_PATH ="/method";
     public static final String AUTH_PATH ="/auth";
-    public static final String GET_PATH ="/auth/get";
-    public static final String POST_PATH = "/auth/post";
-    public static final String PUT_PATH = "/auth/put";
-    public static final String DELETE_PATH = "/auth/delete";
-    public static final String PATCH_PATH = "/auth/patch";
+    public static final String GET_PATH ="/get";
+    public static final String POST_PATH = "/post";
+    public static final String PUT_PATH = "/put";
+    public static final String DELETE_PATH = "/delete";
+    public static final String PATCH_PATH = "/patch";
 
     //USER ROLES
     public static final String ADMIN_ROLE ="ADMIN";
